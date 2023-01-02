38f29464464a0b45070cbdf7b4504438e81f037e
Kevin <weberkm5@gmail.com>
2023-01-02 17:04:10 -0600

    updated player turn, mark board, and displays

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let gameActive = true; 
 let currentPlayer = 'X';
-let gameState = ["", "", "", "", "", "", "", "", "",];
+let state = {
+    board: ["", "", "", "", "", "", "", "", "",],
+    players: ["", ""],
+    counter: 0,
+};
 
 const winningStates = [
     [0,1,2],
@@ -13,24 +17,26 @@ const winningStates = [
     [2,4,6],
 ];
 
-const statusDisplay = document.getElementById('status');
+
 
 const winningMessage = () => {
-    return currentPlayer + "wins!";
+    return currentPlayer + " wins!";
 }
 
 const drawMessage = () => {
     return "It's a draw!"
-}
+};
 
 const currentPlayerTurn = () => {
-    return "It is " + currentPlayer + "turn";
-}
+    return "It is " + currentPlayer +"'s turn";
+};
+
 
 
 // ********** DOM SELECTORS ****************
 // select all cells on the board and store as array
 const cells = document.querySelectorAll('.cell');
+const statusDisplay = document.getElementById('status');
 const restartButton = document.querySelector('.restart');
 const playerTurn = document.querySelector('.player-turn');
 
@@ -55,15 +61,34 @@ function markBoard(event) {
 
     // below function stores the index of the clicked cell in teh clickedcellindex variable using getatrribute method 
     const clickedCellIndex = event.target.getAttribute('data-cell-index');
-    console.log(clickedCellIndex);
+    // console.log(clickedCellIndex);
     
-    // if (gameState[clickedCellIndex] 
+    // if the state.board value is empty, change the state.board at the cell index clicked to the current player value, then display the current player value in the cell clicked on the screen, then check if there is a win, if there is display winning message, if not change the player to the other player
+    if (state.board[clickedCellIndex] === "") {
+        // 
+        state.board[clickedCellIndex] = currentPlayer    
+        event.target.innerHTML = currentPlayer;
+        changePlayer();    
+    } else {
+        return
+    }};
 
-   
+    // this changes the player from X to O, simply by evaluating if the state of current player is X change to O, otherwise we assume the current is O so we will change it to X 
+function changePlayer () {
+    if (currentPlayer === "X") {
+        currentPlayer = "O";
+    } else {
+        currentPlayer = "X";
+    }
+    statusDisplay.innerHTML = currentPlayerTurn();
+};
+
+
+
     // const clickedCell = cellEvent.target;
     // const clickedCellIndex = clickedCell.getAtrribute('data-cell-index');
     // console.log(clickedCellIndex);
-};
+// };
 
 
 // const restartGame = () => {
