0eaba73f391aabc4f553fdcbc6d7d2f7fd07948f
Kevin <weberkm5@gmail.com>
2023-01-02 23:53:13 -0600

    game is fully functional with name display and logic

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-let currentPlayer = 'X';
+let gameActive = false; 
+let currentPlayer;
 let state = {
     board: ["", "", "", "", "", "", "", "", "",],
-    // players: ["", ""],
-    // counter: 0,
+    players: ["", ""],
 };
+
+
 
 const winningStates = [
     [0,1,2],
@@ -25,18 +27,24 @@ const drawMessage = () => {
 };
 
 const currentPlayerTurn = () => {
-    return "It is " + currentPlayer +"'s turn";
+    return "It is " + `${currentPlayer}` +"'s turn";
 };
 
 
 // ********** DOM SELECTORS ****************
 // select all cells on the board and store as array
 const cells = document.querySelectorAll('.cell');
+// console.log(cells);
+
 const currentPlayerDisplay = document.getElementById('status');
 const restartButton = document.querySelector('.restart');
 const playerTurn = document.querySelector('.player-turn');
+const startGameButton = document.querySelector('.startGameButton');
 
-// console.log(cells);
+
+
+
+
 
 // ********** EVENT LISTENERS ****************
 // loop through the cells array and add a click event listener to each, onclick run the cellClicked
@@ -45,9 +53,9 @@ cells.forEach((cell) => {
 });
 // cells.addEventListener('click', console.log('click is working'));
 
-// add a click event listener on the restart button, when clicked execute a function that resets the game to the starting state (currentplayer, board state, player display, and display in individual cells)
 restartButton.addEventListener('click', () => {
-    currentPlayer = "X";
+    gameActive = true;
+    currentPlayer = `${state.players[0]}`;
     state.board = ["", "", "", "", "", "", "", "", "",];
     currentPlayerDisplay.innerHTML = currentPlayerTurn();
     cells.forEach((cell) => {
@@ -55,31 +63,25 @@ restartButton.addEventListener('click', () => {
     });
 });
 
+// here we want to add teh player names to the state.players when inputted
+startGameButton.addEventListener('click', setPlayer);
+
 // ********** FUNCTIONS **************** 
 
-currentPlayerDisplay.innerHTML = currentPlayerTurn();
+// currentPlayerDisplay.innerHTML = currentPlayerTurn();
 
 function cellClicked(event) {
     // console.log('click is working')
     // console.log(event.target);
-    clickedCellEvent = event.target;
+    const clickedCellEvent = event.target;
 
-    // below function stores the index of the clicked cell in teh clickedcellindex variable using getatrribute method 
+    // below function stores the index of the clicked cell in teh clickedcellindex variable using getatrribute method and converts it form a string to a number to pass later
     const clickedCellIndex = parseInt(clickedCellEvent.getAttribute('data-cell-index'));
     // console.log(typeof(clickedCellIndex));
-    // this returned a string and gave me problems, have to change the returned string into a number to use in checkforwin function below
 
-    // if the state.board value is empty, change the state.board at the cell index clicked to the current player value, then display the current player value in the cell clicked on the screen, then check if there is a win, if there is display winning message, if not change the player to the other player
-    // *************************************************************
-    // if (state.board[clickedCellIndex] === "") {
-    //     // 
-    //     state.board[clickedCellIndex] = currentPlayer    
-    //     event.target.innerHTML = currentPlayer;
-    // } else {
-    //     return
-    // }
-
-    if (state.board[clickedCellIndex] !== "") {
+    // if the state.board value is NOT empty (meaning an X or O is already there) or the gameActive has been set to false (meaning the game is over either via a win or a draw) we want to return out and end the function.
+    //  otherwise run the markBoard function (update the stateboard array with X or O, and update the display of the page with X or O) by passing the event target and the index of the cell. then we run the checkforwin (which also checks for draw) and either ends or changes player turn and continues the game
+    if (state.board[clickedCellIndex] !== "" || gameActive === false) {
         return;
     } 
     markBoard(clickedCellEvent, clickedCellIndex);
@@ -87,16 +89,21 @@ function cellClicked(event) {
 };
 
 function markBoard (clickedCellEvent, clickedCellIndex) {
-    state.board[clickedCellIndex] = currentPlayer    
-    clickedCellEvent.innerHTML = currentPlayer;
-}
+    if (currentPlayer === `${state.players[0]}`) {
+    state.board[clickedCellIndex] = "X"    
+    clickedCellEvent.innerHTML = "X";
+    } else {
+        state.board[clickedCellIndex] = "O"    
+        clickedCellEvent.innerHTML = "O";
+    }
+};
 
     // this changes the player from X to O, simply by evaluating if the state of current player is X change to O, otherwise we assume the current is O so we will change it to X 
 function changePlayer () {
-    if (currentPlayer === "X") {
-        currentPlayer = "O";
+    if (currentPlayer === `${state.players[0]}`) {
+        currentPlayer = `${state.players[1]}`;
     } else {
-        currentPlayer = "X";
+        currentPlayer = `${state.players[0]}`;
     }
     currentPlayerDisplay.innerHTML = currentPlayerTurn();
 };
@@ -112,33 +119,33 @@ function checkForWin () {
         const winCondition = winningStates[i];
         // console.log(winCondition[0]);
         let a = state.board[winCondition[0]];
-        console.log('this is a ',a);
+        // console.log('this is a ',a);
         // console.log(typeof(a));
         let b = state.board[winCondition[1]];
-        console.log('this is b ',b);
+        // console.log('this is b ',b);
         let c = state.board[winCondition[2]];
-        console.log('this is c ',c);
+        // console.log('this is c ',c);
         
         // need to cover scenario where values in board are still empty, if so just skip and continue looping
         if (a === "" || b === "" || c === ""){
             continue;
         }
 
-        // if any values in the loop return a, b, and c as equal then that player wins, we need to set the roundwon variable to true , then we need to display the round has been won by the current player who we can assume is teh one who just clicked the cell and triggered all these functions and events. return and leave the function as game is over 
+        // if any values in the loop return a, b, and c as equal then that player wins, we need to set the roundwon variable to true , then we need to display the round has been won by the current player who we can assume is teh one who just clicked the cell and triggered all these functions and events. we need to set the state of the game to false in order reset it later. return and leave the function as game is over 
         if (a === b && b === c) {
             roundWon = true;
         }
     }
     if (roundWon === true) {
         currentPlayerDisplay.innerHTML = winningMessage();
-        
+        gameActive = false;
         return;
     }
       // if there is no winner, we need to check for draw as well. we know there will be a draw if there has been no winner declared (determined by the loop/roundwon above ^^) AND there are no empty spaces left in the state of the board meaning every space has been taken, no winner has been determined, meaning its a draw. return out of the function as the game is over
     let roundDraw = state.board.includes("");
     if (roundDraw === false) {
         currentPlayerDisplay.innerHTML = drawMessage();
-        
+        gameActive = false;
         return;
     }
 // if there is no winner or no draw by the clickevent, then changeplayer and the game continues
@@ -146,14 +153,35 @@ function checkForWin () {
 };
 
 
+// below we run setPlayer after clicking start Game button. Here, if the target element classname is the startGameButton, we select the input element values which are typed by the user, then we assign it to a variable. then we assign the state.player at index 0 and 1 to each input value 
+// then we update current player to = the name inputted in state.player[0] so we have assigned a player to make first move
+// then we assign gameActive to true so the game can offically start ( we locked it before by not allowing event listener to trigger on cells is gameActive = false). and lastly we update the display to show the current players turn on the page!
 
-// function restartGame () {
-//     let currentPlayer = 'X';
-//     let state = {
-//     board: ["", "", "", "", "", "", "", "", "",]
-//     };
-//     currentPlayerDisplay.innerHTML = currentPlayerTurn();
-//     cells.forEach((cell) => {
-//         cell.innerHTML = "";
-//     });
-// };
+function setPlayer(event) {
+    // console.log(event.target.className);
+    // gameActive = true;
+    
+    if (event.target.className === "startGameButton") {
+        
+
+        const player1Input = document.querySelector("input[name=player1]");
+        const player1Value = player1Input.value;
+        state.players[0] = player1Value;
+
+        const player2Input = document.querySelector("input[name=player2]");
+        const player2Value = player2Input.value;
+        state.players[1] = player2Value;
+
+        // console.log('this is current player', currentPlayer);
+        // console.log(currentPlayerTurn());
+
+        // console.log("this is my player1 value", player1Value);
+        // console.log("this is my player2 value", player2Value);
+        // console.log("this is my state.players[0]", state.players[0]);
+        // console.log("this is my state.players[1]", state.players[1]);
+    };
+    gameActive = true;
+    currentPlayer = `${state.players[0]}`;
+    currentPlayerDisplay.innerHTML = currentPlayerTurn();
+};
+
